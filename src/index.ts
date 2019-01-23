e5389d648bf8e50566cd3f653fb0ee8520dce64c
Florian Keller <github@floriankeller.de>
2019-01-23 10:47:58 +0100

    chore: Add prettierrc file

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import * as dgram from 'dgram';
 import * as net from 'net';
-import { StringDecoder } from 'string_decoder';
+import {StringDecoder} from 'string_decoder';
 
 export class Pixelflut {
-  private server: string;
-  private port: number;
+  private readonly server: string;
+  private readonly port: number;
   private udpSocket: dgram.Socket;
   private tcpSocket: net.Socket;
-  private udp: boolean = false;
-  private errorTolerance: number;
-  public errors: Array<string> = [];
+  private readonly udp: boolean = false;
+  private readonly errorTolerance: number;
+  public errors: string[] = [];
 
-  constructor(
-    server: string,
-    port: number,
-    errorTolerance: number = 10,
-    udp: boolean = false
-  ) {
+  constructor(server: string, port: number, errorTolerance: number = 10, udp: boolean = false) {
     this.server = server;
     this.port = port;
     this.udp = udp;
@@ -88,20 +83,13 @@ export class Pixelflut {
 
   private writeToUDP(message: string): Promise<any> {
     return new Promise((resolve, reject) =>
-      this.udpSocket.send(
-        message,
-        0,
-        message.length,
-        this.port,
-        this.server,
-        (err, bytes) => {
-          if (err) {
-            reject(err);
-          } else if (bytes) {
-            resolve(bytes.toString());
-          }
+      this.udpSocket.send(message, 0, message.length, this.port, this.server, (err, bytes) => {
+        if (err) {
+          reject(err);
+        } else if (bytes) {
+          resolve(bytes.toString());
         }
-      )
+      })
     );
   }
 
@@ -111,34 +99,24 @@ export class Pixelflut {
   }
 
   public sendPixel(x: number, y: number, color: string): Promise<string> {
-    console.log(
-      `Sending #${color} at <${x}, ${y}> over ${this.udp
-        ? 'UDP'
-        : 'TCP'} to ${this.server}:${this.port}`
-    );
+    console.log(`Sending #${color} at <${x}, ${y}> over ${this.udp ? 'UDP' : 'TCP'} to ${this.server}:${this.port}`);
 
     const message = `PX ${x} ${y} ${color}\n`;
     return this.createTCPConnection().then(() => this.writeToTCP(message));
   }
 
-  public sendPixels(
-    pixels: Array<{ x: number; y: number; color: string }>
-  ): Promise<any> {
+  public sendPixels(pixels: Array<{x: number; y: number; color: string}>): Promise<any> {
     console.log(
-      `Sending ${pixels.length} pixels from <${pixels[0].x}, ${pixels[
-        pixels.length - 1
-      ].y}> to <${pixels[pixels.length - 1].x}, ${pixels[0].y}> over ${this.udp
-        ? 'UDP'
-        : 'TCP'} to ${this.server}:${this.port}`
+      `Sending ${pixels.length} pixels from <${pixels[0].x}, ${pixels[pixels.length - 1].y}> to <${
+        pixels[pixels.length - 1].x
+      }, ${pixels[0].y}> over ${this.udp ? 'UDP' : 'TCP'} to ${this.server}:${this.port}`
     );
-    const messages: Array<string> = pixels.map(
-      pixel => `PX ${pixel.x} ${pixel.y} ${pixel.color}\n`
-    );
+    const messages: string[] = pixels.map(pixel => `PX ${pixel.x} ${pixel.y} ${pixel.color}\n`);
 
     return this.createTCPConnection().then(() =>
-      Promise.all(
-        messages.map(message => this.writeToTCP(message))
-      ).then(values => values.filter(value => typeof value !== 'undefined'))
+      Promise.all(messages.map(message => this.writeToTCP(message))).then(values =>
+        values.filter(value => typeof value !== 'undefined')
+      )
     );
   }
 
